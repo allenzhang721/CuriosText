3c1aef49741177e9dbd649affeb31b55c946cb8e
Emiaostein <cszwdy@gmail.com>
2016-03-03 14:48:04 +0800

    Update - Add Retrive Data from Disk for BlackCatManager

--- CuriosTextApp/CuriosTextApp/BlackCat/Extension+BlackCatManager.swift
+++ CuriosTextApp/CuriosTextApp/BlackCat/Extension+BlackCatManager.swift
@@ -9,28 +9,21 @@
 import Foundation
 extension BlackCatManager {
     
-    func retriveDataForURL(urlString:String, completedHandler: ((NSData?) -> ())?) {
+    func retriveDataForURL(url: NSURL, completedHandler: ((NSData?) -> ())?) {
         
-        let key = urlString
+        let key = url.absoluteString
         let targetCache = self.cache
         let data = targetCache.retrieveDataInDiskCacheForKey(key)
         completedHandler?(data)
     }
     
-    func storeData(data: NSData, byURL urlString:String, completedHandler:((Bool) -> ())?) {
-        let key = urlString
+    func storeData(data: NSData, byURL url: NSURL, completedHandler:((Bool) -> ())?) {
+        let key = url.absoluteString
         let targetCache = self.cache
-        let oldData = targetCache.retrieveDataInDiskCacheForKey(key)
-        if oldData == nil {
+        targetCache.removeDataForKey(key, fromDisk: true, completionHandler: {
             targetCache.storeData(data, forKey: key, toDisk: true, completionHandler: {
                 completedHandler?(true)
             })
-        }else {
-            targetCache.removeDataForKey(key, fromDisk: true, completionHandler: {
-                targetCache.storeData(data, forKey: key, toDisk: true, completionHandler: {
-                    completedHandler?(true)
-                })
-            })
-        }
+        })
     }
 }
