cef2acb248fa2c7e11809d8e5fe4ff8b4bde5e93
Emiaostein <cszwdy@gmail.com>
2016-03-09 17:55:14 +0800

    Update - Local String.

--- CuriosTextApp/CuriosTextApp/LocalizationManager.swift
+++ CuriosTextApp/CuriosTextApp/LocalizationManager.swift
@@ -10,13 +10,60 @@ import Foundation
 
 enum LocalStrings: CustomStringConvertible {
 
-    case EditTextPlaceHolder
+    case Cancel, Done         // Common
+    case Camera, Photo  // Image Picker
+    case Publish , EditTextPlaceHolder  // Editor
+    case Size, Rotation, Font, Spacing, Alignment, Color, Animation, AnimationType, AnimationDuration, AnimationDelay    // Edior Tab
+    case MoveIn, MoveOut //Animations
     
     var description: String {
         
         switch self {
+            // Common
+        case .Cancel:
+            return NSLocalizedString("Cancel", comment: "")
+        case .Done:
+            return NSLocalizedString("Done", comment: "")
+            
+            // Image Picker
+        case .Camera:
+            return NSLocalizedString("Camera", comment: "")
+        case .Photo:
+            return NSLocalizedString("Photo", comment: "")
+            
+            // Editor
+        case .Publish:
+            return NSLocalizedString("Publish", comment: "")
         case .EditTextPlaceHolder:
             return NSLocalizedString("EditTextPlaceHolder", comment: "")
+            
+            // Edior Tab
+        case .Size:
+            return NSLocalizedString("Size", comment: "")
+        case .Rotation:
+            return NSLocalizedString("Rotation", comment: "")
+        case .Font:
+            return NSLocalizedString("Font", comment: "")
+        case .Spacing:
+            return NSLocalizedString("Spacing", comment: "")
+        case .Alignment:
+            return NSLocalizedString("Alignment", comment: "")
+        case .Color:
+            return NSLocalizedString("Color", comment: "")
+        case .Animation:
+            return NSLocalizedString("Animation", comment: "")
+        case .AnimationType:
+            return NSLocalizedString("AnimationType", comment: "")
+        case .AnimationDuration:
+            return NSLocalizedString("AnimationDuration", comment: "")
+        case .AnimationDelay:
+            return NSLocalizedString("AnimationDelay", comment: "")
+            
+            // Animation
+        case .MoveIn:
+            return NSLocalizedString("MoveIn", comment: "")
+        case .MoveOut:
+            return NSLocalizedString("MoveOut", comment: "")
         }
         
     }
