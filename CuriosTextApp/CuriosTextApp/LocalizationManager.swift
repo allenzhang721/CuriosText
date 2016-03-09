70b898ee1ce95be34e795781ee21d9dbb0293f01
Emiaostein <cszwdy@gmail.com>
2016-03-09 10:33:43 +0800

    Update - Add LocalStringManager

---
+++ CuriosTextApp/CuriosTextApp/LocalizationManager.swift
@@ -0,0 +1,24 @@
+//
+//  LocalizationManager.swift
+//  CuriosTextApp
+//
+//  Created by Emiaostein on 3/9/16.
+//  Copyright © 2016 botai. All rights reserved.
+//
+
+import Foundation
+
+//enum LocalStrings: CustomStringConvertible {
+
+    // Editor
+//    case TextPlaceHolder
+//    
+//    var description: String {
+//        
+//        switch self {
+//        case .EditTextPlaceHolder:
+//            return NSLocalizedString("AlertTitleInternetError", comment: "")
+//        }
+//        
+//    }
+//}
