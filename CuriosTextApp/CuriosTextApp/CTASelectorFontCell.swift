190928cb62750265499bb41601af36005982884f
Emiaostein <cszwdy@gmail.com>
2016-04-12 11:16:43 +0800

    Update - Selector Inset Left and Right Margin.

--- CuriosTextApp/CuriosTextApp/CTASelectorFontCell.swift
+++ CuriosTextApp/CuriosTextApp/CTASelectorFontCell.swift
@@ -18,14 +18,15 @@ final class CTASelectorFontCell: CTASelectorCell {
     }
     
     private func setup() {
-        view = CTAPickerView(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: UIScreen.mainScreen().bounds.width, height: 88)), showCount: 2)
+        view = CTAPickerView(frame: CGRect(origin: CGPoint.zero, size: CGSize(width: UIScreen.mainScreen().bounds.width - 40, height: 88)), showCount: 2)
+        view.collectionView.clipsToBounds = false
         view.backgroundColor = UIColor.whiteColor()
         contentView.addSubview(view)
         view.backgroundColor = CTAStyleKit.intoDreams1
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.leadingAnchor.constraintEqualToAnchor(leadingAnchor).active = true
+        view.leadingAnchor.constraintEqualToAnchor(leadingAnchor, constant: 20).active = true
         view.topAnchor.constraintEqualToAnchor(topAnchor).active = true
-        view.trailingAnchor.constraintEqualToAnchor(trailingAnchor).active = true
+        view.trailingAnchor.constraintEqualToAnchor(trailingAnchor, constant: -20).active = true
         view.bottomAnchor.constraintEqualToAnchor(bottomAnchor).active = true
         
         view.dataSource = self
