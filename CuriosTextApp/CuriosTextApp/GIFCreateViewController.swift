0b195406faffa24afe734b59f5270875189e3a84
Emiaostein <cszwdy@gmail.com>
2016-05-05 18:01:59 +0800

    Update - update gif.

--- CuriosTextApp/CuriosTextApp/GIFCreateViewController.swift
+++ CuriosTextApp/CuriosTextApp/GIFCreateViewController.swift
@@ -15,9 +15,11 @@ class GIFCreateViewController: UIViewController {
     var canvas: AniCanvas!
     var imageRetriver: ((String, (String, UIImage?) -> ()) -> ())?
     var aniCanvasView: AniPlayCanvasView!
+    var progressBlock: ((progress: CGFloat, next: () -> ()) -> ())?
+    var indexs = [Range<Int>]()
+    var currentIndex = 0
+    var counts = 0
     var completed: ((NSURL, UIImage) -> ())?
-    
-    var v: UIView!
     
     override func prefersStatusBarHidden() -> Bool {
         return true
@@ -25,83 +27,96 @@ class GIFCreateViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         setupViews()
         setupStyles()
-        
-        v = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
-        v.backgroundColor = UIColor.lightGrayColor()
-        view.addSubview(v)
-    }
-    
-    override func viewWillAppear(animated: Bool) {
-        
     }
     
     override func viewDidAppear(animated: Bool) {
-        
         dispatch_async(dispatch_get_main_queue(), {
             self.aniCanvasView.reloadData { [weak self] in
                 guard let sf = self else { return }
                 dispatch_async(dispatch_get_main_queue(), {
                     sf.aniCanvasView.ready()
+                    
                     dispatch_async(dispatch_get_main_queue()) { [weak self] in
                         guard let sf = self else { return }
-                        
-                        UIView.animateWithDuration(5, animations: {
-                            sf.v.frame = CGRect(x: 0, y: 568, width: 50, height: 50)
-                        })
-                        
-                        sf.makeGIF()
+                        sf.makeGIFBegan()
+                        //                        sf.makeGIF()
                     }
                 })
             }
         })
-        
-        
     }
     
-    private func makeGIF() {
-        
+    private func next() {
+        print("next")
+        let index = indexs[currentIndex]
+        makeGIF(index)
+    }
+    
+    private func makeGIFBegan() {
         let result = aniCanvasView.progressBegan()
         
         switch result {
         case .Next(let duration):
             let FPS = 24
-            let count = Int(CGFloat(FPS) * duration) + 1
+            counts = Int(CGFloat(FPS) * duration)
+            var c = counts
+            var i = 0
+            while c > 24  {
+                let ind = i..<(i + 24)
+                i += 24
+                c -= 24
+                indexs.append(ind)
+            }
+            
+            if c > 0 {
+                indexs.append(i..<i + c)
+            }
+            
+            debug_print(indexs)
             
             GIFCreator.beganWith(publishID, images: [], delays: [], ignoreCache: true)
             
-            dispatch_async(dispatch_get_main_queue()) { [weak self] in
-                guard let sf = self else {return}
-                var thumbImage: UIImage!
-                for i in 0..<count {
-                    let progress = CGFloat(i) / CGFloat(count)
-                    sf.aniCanvasView.progress =  progress < 1.0 ? progress : 1.0
-                    let snapshot = sf.aniCanvasView.snapshotViewAfterScreenUpdates(true)
-//                    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)) {
-                    autoreleasepool {
-                        UIGraphicsBeginImageContext(CGSize(width: 320, height: 320))
-                        sf.aniCanvasView.drawViewHierarchyInRect(CGRect(origin: CGPoint.zero, size: CGSize(width: 320, height: 320)), afterScreenUpdates: true)
-                        let image = UIGraphicsGetImageFromCurrentImageContext()
-                        UIGraphicsEndImageContext()
-                        
-                        let img = UIImage(data: UIImageJPEGRepresentation(image, 0.1)!)!
-                        if i == count - 1 { thumbImage = img }
-                        GIFCreator.addImage(img, delay: 1.0 / CGFloat(FPS))
-                    }
-                    }
-//                }
-                GIFCreator.commitWith({[weak self] (url) in
-                    debug_print("gif url = \(url)")
-                    self?.completed?(url, thumbImage)
-                    })
-            }
-            
-            
+            next()
             
         default:
             ()
+        }
+    }
+    
+    private func makeGIF(indexs: Range<Int>) {
+        let count = counts
+        
+        
+        var thumbImage: UIImage!
+        for i in indexs {
+            let progress = CGFloat(i) / CGFloat(count)
+            aniCanvasView.progress =  progress < 1.0 ? progress : 1.0
+            autoreleasepool {
+                UIGraphicsBeginImageContext(CGSize(width: 320, height: 320))
+                aniCanvasView.drawViewHierarchyInRect(CGRect(origin: CGPoint.zero, size: CGSize(width: 320, height: 320)), afterScreenUpdates: true)
+                let image = UIGraphicsGetImageFromCurrentImageContext()
+                UIGraphicsEndImageContext()
+                
+                let img = UIImage(data: UIImageJPEGRepresentation(image, 0.1)!)!
+                if i == count - 1 { thumbImage = img }
+                GIFCreator.addImage(img, delay: 1.0 / CGFloat(24))
+            }
+        }
+        
+        currentIndex += 1
+        if currentIndex < self.indexs.count {
+            if let progressBlock = progressBlock {
+                progressBlock(progress: aniCanvasView.progress, next: next)
+            } else {
+                next()
+            }
+        } else {
+            GIFCreator.commitWith({[weak self] (url) in
+                debug_print("gif url = \(url)")
+                self?.completed?(url, thumbImage)
+            })
         }
     }
 }
@@ -114,19 +129,10 @@ extension GIFCreateViewController {
         aniCanvasView.backgroundColor = UIColor.groupTableViewBackgroundColor()
         aniCanvasView.dataSource = canvas
         aniCanvasView.aniDataSource = canvas
-        //        view.layer.addSublayer(aniCanvasView.layer)
-        
-        //        for c in canvas.containers {
-        //            if let content = c.contents.first where content.type == .Image {
-        //                c.imageRetriver = imageRetriver
-        //            }
-        //        }
-        
         view.addSubview(aniCanvasView) // debug
         if let fakeView = fakeView {
             view.addSubview(fakeView)
         }
-        
     }
     
     func setupStyles() {
@@ -139,5 +145,6 @@ extension GIFCreateViewController {
         
         let scale = min(view.bounds.size.width / canvas.size.width, view.bounds.size.height / canvas.size.height)
         aniCanvasView.transform = CGAffineTransformMakeScale(scale, scale)
+        aniCanvasView.center = view.center
     }
 }
