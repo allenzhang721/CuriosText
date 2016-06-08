05a8c2557fcdd69ddf6d77692d3a92a4235d2fe5
Emiaostein <cszwdy@gmail.com>
2016-06-08 13:24:42 +0800

    Update - Gif Loop.

--- CuriosTextApp/CuriosTextApp/GIFCreator.swift
+++ CuriosTextApp/CuriosTextApp/GIFCreator.swift
@@ -82,7 +82,7 @@ class GIFCreator {
         let count = instance.images.count
         
         let destination = CGImageDestinationCreateWithURL(instance.fileURL, kUTTypeGIF, count, nil)!
-        let gifProperties = [(kCGImagePropertyGIFLoopCount as String): 0]
+        let gifProperties = [(kCGImagePropertyGIFDictionary as String):[(kCGImagePropertyGIFLoopCount as String): 0]]
         CGImageDestinationSetProperties(destination, gifProperties)
         
         dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)) {
